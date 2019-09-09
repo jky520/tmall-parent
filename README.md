67f35b191c78b459689c1a73e1e9b57084e693c0
jky520 <jky1988@qq.com>
2019-09-09 17:13:33 +0800

    微信端

---
+++ README.md
@@ -0,0 +1,2 @@
+# 多模块集成微信框架  
+## 1、spring boot 多模块集成weixin-java-mp
---
+++ tmall-weixin-web/src/main/java/com/hx/tmall/TmallWeixinWebApplication.java
@@ -0,0 +1,13 @@
+package com.hx.tmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmallWeixinWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmallWeixinWebApplication.class, args);
+    }
+
+}
